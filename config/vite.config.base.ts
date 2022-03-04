7bd33e761b3655e2977bfec8f7ada51ab62b79bf
chuzhixin <1204505056@qq.com>
2022-03-04 22:39:46 +0800

    🚀 feat: build

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -5,7 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
-  base: '/vue-admin-arco/',
+  base: '',
   plugins: [vue(), vueJsx(), svgLoader({ svgoConfig: {} })],
   resolve: {
     alias: [
