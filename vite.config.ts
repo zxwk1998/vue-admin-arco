f547814121db0b7a2ea34cd7bb4ad7766ab9c65a
chuzhixin <1204505056@qq.com>
2022-03-04 22:17:06 +0800

    🚀 feat: build

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import type { UserConfig, ConfigEnv } from 'vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
+  return {
+    base: '/vue-admin-arco',
+  };
+});
