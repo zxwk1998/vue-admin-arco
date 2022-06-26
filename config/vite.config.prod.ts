9f97a84a0b183cc0bff01f690d12fd6378c5bc81
myqz <ambition292@163.com>
2022-06-26 14:41:41 +0800

    fix: build baseUrl

--- config/vite.config.prod.ts
+++ config/vite.config.prod.ts
@@ -1,9 +1,9 @@
 import { mergeConfig } from 'vite';
-import baseConig from './vite.config.base';
+import baseConfig from './vite.config.base';
 
 export default mergeConfig(
   {
     mode: 'production',
   },
-  baseConig
+  { ...baseConfig, base: '/' }
 );
