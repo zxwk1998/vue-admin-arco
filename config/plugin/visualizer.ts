e4bddf65cb27b434f29f2c60878420bc4d832e96
zhideyiman <chu1204505056@126.com>
2023-11-18 16:50:13 +0800

    deploy

--- config/plugin/visualizer.ts
+++ config/plugin/visualizer.ts
@@ -3,16 +3,12 @@
  * 生成打包分析
  */
 import visualizer from 'rollup-plugin-visualizer'
-import { isReportMode } from '../utils'
 
 export default function configVisualizerPlugin() {
-  if (isReportMode()) {
-    return visualizer({
-      filename: './node_modules/.cache/visualizer/stats.html',
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    })
-  }
-  return []
+  return visualizer({
+    filename: './node_modules/.cache/visualizer/stats.html',
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+  })
 }
